4b15757aea4dc2d2329f32047e6695509ec375ef
jGo-java-General-operations <staff@jgo.cloud>
2018-11-20 14:59:01 +0100

    Aggiornamento javadoc

--- jGo/src/cloud/jgo/£.java
+++ jGo/src/cloud/jgo/£.java
@@ -96,6 +96,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -131,8 +132,11 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 import javax.xml.bind.JAXBException;
+
 import org.junit.internal.runners.model.EachTestNotifier;
+
 import com.google.gson.Gson;
+
 import cloud.jgo.SMTPHosts.SMTPEntry;
 import cloud.jgo.downloads.Download;
 import cloud.jgo.downloads.DownloadWorker;
@@ -149,6 +153,7 @@ import cloud.jgo.net.tcp.TCPServerTypes;
 import cloud.jgo.net.tcp.http.HTTPServer;
 import cloud.jgo.net.tcp.http.HTTPServerConfiguration;
 import cloud.jgo.net.tcp.http.headers.Header;
+import cloud.jgo.net.tcp.http.jor.JOR;
 import cloud.jgo.net.tcp.http.jor.JORServer;
 import cloud.jgo.net.tcp.login.TCPLoginServer;
 import cloud.jgo.registry.WinRegistry;
@@ -162,35 +167,128 @@ import cloud.jgo.utils.command.terminal.phase.LocalPhaseTerminal;
  * @see £#_S
  * @see £#_W
  * @version 1.0.5
-   <!--<link rel='styleSheet' href='https://www.jgo.cloud/docStyle.css'> -->
+   <!--<link rel='styleSheet' href='https://www.jgo.cloud/docStyle.css'> --><br>
     <!--Author : *** Marco Martire *** -->  
-   <h1 style='color: #282828;'>jGo<span style='color: green;'>.cloud</span></h1>
-   <img id='logo'src='https://www.jgo.cloud/wp-content/uploads/2018/11/jgo2.png' alt='logo jgo'><br>
+     <img id='logo'src='https://www.jgo.cloud/wp-content/uploads/2018/11/jgo2.png' alt='logo jgo' style='float: left;margin-right:15px;'><br>
+   <h1 style='color: #282828;'>jGo<strong style='color: green;'>.cloud</strong></h1>
+   <em>java General operations</em><br><br><br>
    <strong>Description :</strong><br> This class facilitates all the difficult tasks
-   For example,In a code line, you take a screenshot.<br>
-   JGO is very powerful and it follows the jquery policy :<br>
-   <em><u>(Write a little and get a lot)</u></em><br>
-   <strong>J</strong> - <em>Java</em><br>
-   <strong>G</strong> - <em>General</em><br>
-   <strong>O</strong> - <em>Operations</em><br><br>
-   <strong>1 Example</strong>  -  <em class='explanations'>Prints "Welcome in JGO", executes a screenshot with a final function and finally emits an acoustic signal :</em> : <br>
-    public static void main(<em class='type'>String</em>[]args){<br>
-    <em class='type'>£</em>._O("Welcome in JGO").<em class='method'>screenshot</em>(new <em class='type'>£Func</em>() {<br>
-    <strong style='color:darkgray'>@Override</strong><br>
-    public Object <em style='#303030'>function</em>(<em class='type'>Object</em> e) {<br>
-    <em class='type'>£</em>.<em class='method'>alert</em>("Hello World");<br>
-    return null ;<br>
-    }}).<em class='method'>beep</em>().<em class='method'>beep</em>();<br>
-    &nbsp;}
-    <br><br>
-    <strong>2 Example</strong>  -  <em class='explanations'>Emits an acoustic signal and opens the windows cmd for 3 times and opens windows notepad :</em><br>
-    <em class='type'>£</em>.<em class='method'>beep</em>().<em class='method'>openCmd</em>(3).<em class='method'>openNotepad</em>();
-    <br><br>
-    <strong>3 Example</strong>  -  <em class='explanations'>Slide panel :</em><br>
-    <em class='type'>£</em>._<span style='color: #006200;'>S</span>.<em class='method'>slide</em>(panel,Effect.<span style='color: #006200;'>VERTICAL</span>,Effect.<span style='color: #006200;'>FAST</span>);
-    <br><br>
-    <strong>4 Example</strong>  -  <em class='explanations'>Moves two times the frame :</em><br>
-    <em class='type'>£</em>._<em style='color:#282828;'>S</em>.<em class='method'>moveJFrame</em>(frm, 0, 0,<span style='color: #006200;'>Effect</span>.SLOW).<em class='method'>moveJFrame</em>(frm, 300, 300, Effect.<span style='color: #006200;'>FAST</span>);
+   For example,In a code line, you take a screenshot, and do other general operations. 
+   the calls to the methods can be concatenated, so that different operations can be performed in the same programmatic instruction.
+   jGo was born to facilitate the life of the java programmer, besides it provides different technologies like {@link JjDom} and {@link JON} for example.
+   jGo also allows you to go to specific phases for specific programming +, for example if our application is a JFrame, we can find the general methods in the _S phase, object of the SwingUtils class
+   Moreover jGo contains 3 phases for specific programming, for example if our application is a {@link JFrame}, we will find the general Swing methods in the {@link £#_S} phase, static constant of type {@link SwingUtils}.
+   However the WebUtils and AndroidUtils phases are still empty. The goal is to create a jar package comprising a wide range of general methods available to the user, so as to facilitate the life of the programmer, increase the performance of the program, and not less important, save a lot of time in terms of writing of the code.
+   Moreover jGo provides very powerful objects, for example it is possible to create very powerful servers, for the moment servers whose protocol must be tcp, or an extension like http.
+   Study the following server creation packages well:<br>
+   <ul>
+   <li><a href='#'>cloud.jgo.net</a> - contains all important interfaces, including the {@link Server} interface</li>
+   <li><a href='#'>cloud.jgo.net.tcp</a> - contains all the tcp classes needed to implement a TCP server - {@link TCPServer}
+   <li><a href='#'>cloud.jgo.net.http</a> - contains all the http classes needed to implement a HTTP server - {@link HTTPServer}
+   <li><a href='#'>cloud.jgo.net.http.jor</a> - contains all the classes necessary for the implementation of a server with JOR technology - {@link JORServer}
+   </ul>
+   Or we can implement a terminal thanks to the objects contained in the package <a href='#'>cloud.jgo.utils.command</a> and <a href='#'>cloud.jgo.utils.command.terminal</a>. 
+   Then we have {@link JjDom}, a technology that allows us to create an HTML document and write <a href='https://jquery.com/'>jquery</a> instructions in java. Furthermore we can of course write the css code necessary for our document.
+   To use jGo, read the <a href='https://www.jgo.cloud/cookbook'>cookbook</a> and consult the website at <a href='https://www.jgo.cloud/'>https://www.jgo.cloud/</a>. 
+   <br>
+   <em>Have fun !</em>
+   <br><br>
+   JGO is very powerful and it follows the <a href='https://jquery.com/'>jquery</a> policy :<br><br>
+   <em><u><a href='https://medium.com/laboratoria-how-to/write-less-do-more-e049d0824f4'>(Write a little and get a lot)</a></u></em><br><br>
+   <strong>j</strong> - <em>java</em><br><br>
+   <strong>G</strong> - <em>General</em><br><br>
+   <strong>o</strong> - <em>operations</em><br><br>
+   <hr>
+   <strong>1 Example-</strong>This instruction prints in the console "hello world" and "hello world 2",<br>also increases and decreases twice a counter:<br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 | &pound;._O(</code><code class="cm_n_n_n_2A00FF">&quot;Hello&nbsp;world&quot;</code><code class="cm_n_n_n_0">).increment().decrement()<br>
+  2 |&nbsp;  ._O(</code><code class="cm_n_n_n_2A00FF">&quot;Hello&nbsp;World&nbsp;2&quot;</code><code class="cm_n_n_n_0">).increment().decrement();</code>
+</div>
+   <br>
+   <strong>2 Example -</strong>
+   Opens the terminal, opens the notepad and emits an acoustic signal,<br>also prints in the console "Hello world":<br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 | &pound;.openTerminal().openNotepad().beep()._O(</code><code class="cm_n_n_n_2A00FF">&quot;Hello&nbsp;World&quot;</code><code class="cm_n_n_n_0">);</code>
+</div>
+<br>
+ <strong>3 Example -</strong>This instruction creates a file, a folder,<br>and prints hello world using two alert windows:<br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 | &pound;.createFile(</code><code class="cm_n_n_n_2A00FF">&quot;hello&nbsp;world&quot;</code><code class="cm_n_n_n_0">).md().alert(</code><code class="cm_n_n_n_2A00FF">&quot;Hello&quot;</code><code class="cm_n_n_n_0">).alert(</code><code class="cm_n_n_n_2A00FF">&quot;World&quot;</code><code class="cm_n_n_n_0">);</code>
+</div>
+<br>
+<strong>4 Example (Iteration with jGo) - </strong>Simple use of the "each" method of jGo:<br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 &nbsp;| String[]arr&nbsp;=&nbsp;{</code><code class="cm_n_n_n_2A00FF">&quot;Alan&quot;</code><code class="cm_n_n_n_0">,</code><code class="cm_n_n_n_2A00FF">&quot;Peter&quot;</code><code class="cm_n_n_n_0">,</code><code class="cm_n_n_n_2A00FF">&quot;Lucas&quot;</code><code class="cm_n_n_n_0">};<br>
+  2 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  3 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&pound;.each(arr,</code><code class="cm_b_n_n_7F0055">new</code><code class="cm_n_n_n_0">&nbsp;&pound;Func()&nbsp;{<br>
+  4 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  5 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@Override<br>
+  6 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="cm_b_n_n_7F0055">public</code><code class="cm_n_n_n_0">&nbsp;Object&nbsp;function(Object&nbsp;e)&nbsp;{<br>
+  7 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  8 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;System.out.println(</code><code class="cm_n_n_n_2A00FF">&quot;Name:&quot;</code><code class="cm_n_n_n_0">+e);<br>
+  9 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+ 10 |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="cm_b_n_n_7F0055">return</code><code class="cm_n_n_n_0">&nbsp;</code><code class="cm_b_n_n_7F0055">true</code><code class="cm_n_n_n_0">&nbsp;;<br>
+ 11 |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}<br>
+ 12 |&nbsp;&nbsp;&nbsp;&nbsp;});</code>
+</div><br>
+<strong>5 Example - </strong>Obviously more complex instructions can be combined <br>
+Increment the counter twice, create a file, iterate the array using the "each" method,
+at the end it queries on google, decreases the value of the counter, and takes its value:<br><br>
+<div class="cm_source">
+<code class="cm_b_n_n_7F0055">1 &nbsp;| int</code><code class="cm_n_n_n_0">&nbsp;counterValue&nbsp;=&nbsp;<br>
+  2 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  3 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&pound;.increment().increment().createFile(</code><code class="cm_n_n_n_2A00FF">&quot;test.json&quot;</code><code class="cm_n_n_n_0">)<br>
+  4 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.each(arr,</code><code class="cm_b_n_n_7F0055">new</code><code class="cm_n_n_n_0">&nbsp;&pound;Func()&nbsp;{<br>
+  5 &nbsp;|
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  6 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@Override<br>
+  7 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="cm_b_n_n_7F0055">public</code><code class="cm_n_n_n_0">&nbsp;Object&nbsp;function(Object&nbsp;e)&nbsp;{<br>
+  8 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  9 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&pound;._O(</code><code class="cm_n_n_n_2A00FF">&quot;Name&nbsp;:&quot;</code><code class="cm_n_n_n_0">+e);<br>
+  10 |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  11 |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="cm_b_n_n_7F0055">return</code><code class="cm_n_n_n_0">&nbsp;</code><code class="cm_b_n_n_7F0055">true</code><code class="cm_n_n_n_0">&nbsp;;<br>
+  12 |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}<br>
+  13 |&nbsp;&nbsp;&nbsp;&nbsp;}).executeGoogleQuery(</code><code class="cm_n_n_n_2A00FF">&quot;jGo&quot;</code><code class="cm_n_n_n_0">).decrement().value();</code>
+</div>
+<br>
+<strong>In jGo there are 3 phases :</strong><br><br>
+<ul>
+<li>{@link #_A} - {@link AndroidUtils} - (<code>cloud.jgo.£.AndroidUtils</code>) - <em style="color: #303030;">&nbsp;for <a href='https://it.wikipedia.org/wiki/Android'>android</a> programming</em></li>
+<li>{@link #_S} - {@link SwingUtils} - (<code>cloud.jgo.£.SwingUtils</code>) - <em style="color: #303030;">&nbsp;for <a href='https://it.wikipedia.org/wiki/Swing_(Java)'>swing</a> programming</em></li>
+<li>{@link #_W} - {@link WebUtils} - (<code>cloud.jgo.£.WebUtils</code>) - <em style="color: #303030;">&nbsp;for <a href='https://it.wikipedia.org/wiki/World_Wide_Web'>web</a> programming</em></li>
+</ul><br>
+For example, let's see how to apply some swing methods :<br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 | &pound;.<strong style='color: blue;'>_S</strong>.hide(jframe,Effect.<strong style='color: blue;'>SLOW</strong>).show(jframe,&nbsp;Effect.<strong style='color: blue;'>SLOW</strong>);</code>
+</div>
+<br>
+We can associate a click event to the button :<br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 &nbsp;| &pound;.<strong class='costants'>_S</strong>.click(button,</code><code class="cm_b_n_n_7F0055">new</code><code class="cm_n_n_n_0">&nbsp;&pound;Func()&nbsp;{<br>
+2 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+3 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@Override<br>
+4 &nbsp;| &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="cm_b_n_n_7F0055">public</code><code class="cm_n_n_n_0">&nbsp;Object&nbsp;function(Object&nbsp;e)&nbsp;{<br>
+5 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+6 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+7 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="cm_b_n_n_7F0055">return</code><code class="cm_n_n_n_0">&nbsp;&pound;.alert(</code><code class="cm_n_n_n_2A00FF">&quot;Hello&nbsp;world&quot;</code><code class="cm_n_n_n_0">);<br>
+8 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+9 &nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}<br>
+10 |&nbsp;&nbsp;&nbsp;&nbsp;});</code>
+</div><br>
+<strong>In any Phase we find ourselves, we can return to the access point using the "home ()" method :</strong><br><br>
+<div class="cm_source">
+<code class="cm_n_n_n_0">1 | &pound;.<strong style='color: blue;'>_S</strong>.applyEffect(Effect.<strong style='color: blue;'>CRAZY</strong>,jframe,jframe2,jframe3)<br>
+2 |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.home().alert(</code><code class="cm_n_n_n_2A00FF">&quot;Returned&quot;</code><code class="cm_n_n_n_0">);</code>
+</div>
+<br>
+<strong>Now let's move on to the jGo technologies :</strong><br><br>
+<ul>
+<li>{@link JON} - <em style='color:  #00593B;'>Java Object Notation</em> - (<code>cloud.jgo.io.jon</code>)</li>
+<li>{@link JOR} - <em style='color:  #00593B;'>Java Object Representation</em> - (<code>cloud.jgo.io.jon</code>)</li>
+<li>{@link JjDom} - <em style='color:  #00593B;'>Java <a href='#'>jQuery</a> Dom</em> - (<code>cloud.jgo.io.jon</code>)</li>
+</ul>
+<br>
+<em>Visit the jGo website</em> : <a href='https://www.jgo.cloud/'>https://www.jgo.cloud/</a>
  */
 public class £{
 	/**
