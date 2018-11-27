0b0338bb36002a134515faafd310dc33d2474657
jGo-java-General-operations <staff@jgo.cloud>
2018-11-27 16:28:57 +0100

    Ultime modifiche prima del rilascio 1.0.6

--- jGo/src/cloud/jgo/test/ConfigTest.java
+++ jGo/src/cloud/jgo/test/ConfigTest.java
@@ -1,42 +1,12 @@
 package cloud.jgo.test;
-
-import java.awt.AWTException;
-import java.awt.HeadlessException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.SocketException;
-import java.security.AllPermission;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Properties;
-import javax.xml.bind.annotation.XmlRootElement;
-import cloud.jgo.£;
-import cloud.jgo.£Func;
-import cloud.jgo.net.ServerTypes;
-import cloud.jgo.net.ServersUtils;
-import cloud.jgo.net.config.Configuration;
-import cloud.jgo.net.config.Configuration.ConfigurationKey;
-import cloud.jgo.net.config.ServerConfiguration;
-import cloud.jgo.net.factorys.ServersFactory;
-import cloud.jgo.net.handlers.Handler;
-import cloud.jgo.net.tcp.TCPHandlerConnection;
-import cloud.jgo.net.tcp.TCPServer;
-import cloud.jgo.net.tcp.TCPServerConfiguration;
-import cloud.jgo.net.tcp.TCPServerTypes;
-import cloud.jgo.net.tcp.http.HTTPHandlerConnection;
-import cloud.jgo.net.tcp.http.HTTPRequest;
-import cloud.jgo.net.tcp.http.HTTPResponse;
-import cloud.jgo.net.tcp.http.HTTPServer;
-import cloud.jgo.net.tcp.http.HTTPServerConfiguration;
-import cloud.jgo.net.tcp.http.jor.JORServer;
-import cloud.jgo.net.tcp.login.TCPLoginServer;
-import cloud.jgo.net.tcp.login.TCPLoginServerConfiguration;
+import cloud.jgo.j£;
+import cloud.jgo.io.File;
 public class ConfigTest {
-public static void main(String[] args) throws SocketException {
+public static void main(String[] args){
+	
+	
+		// okok possiamo documentare e rilasciare
+		// la versione 1.0.6
 	
 	
 }
