1651d5974a1ea705fee4bb4a5cab1e9fd22db3fb
jGo-java-General-operations <staff@jgo.cloud>
2018-12-03 19:43:28 +0100

    The first commit of this branch

---
+++ jGo/src/cloud/jgo/test/UnifyTest.java
@@ -0,0 +1,22 @@
+package cloud.jgo.test;
+
+public class UnifyTest {
+public static void main(String[] args) {
+	
+	// okok aggiungiamo questa nuova possibilità 
+	// per i comandi, può capitare che si vuole
+	// associare un parametro a un comando, in 
+	// tal modo, quando si inserisce il comando
+	// in realtà si sta inserendo il comando + il parametro
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
