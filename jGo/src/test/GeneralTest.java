8643181e1e33ffe4356963fc42ea4cfbfe91c8b6
jGo-java-General-operations <staff@jgo.cloud>
2018-12-19 22:41:29 +0100

    Last commit for today

--- jGo/src/test/GeneralTest.java
+++ jGo/src/test/GeneralTest.java
@@ -3,18 +3,17 @@ package test;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.fusesource.jansi.Ansi.Color;
+
+import cloud.jgo.j£;
 import cloud.jgo.utils.command.LocalCommand;
 import cloud.jgo.utils.command.terminal.LocalTerminal;
 
 public class GeneralTest {
 public static void main(String[] args) {
 	
-	// 2 bug da risolvere: controllare poi il bug della creazione dei nodi personali
-		// dico in html. Un altra cosa cosa succede se diamo al comando createElement
-		// un valore pari a null, parlo sia in html che xml - quindi verificare ...
-	// altro bug : lavorare sul colore dei commenti
-	// come prima cosa, capire problema colore markup
-	// 3 bug : devo adattare L'HelpCommand che comunque mi ordina i parametri
+	
+	
 	
 }
 }
