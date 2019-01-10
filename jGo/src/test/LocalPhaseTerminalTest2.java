07ab4ccc63625dd3ab8191651c50ad0a960a2b7c
jGo-java-General-operations <staff@jgo.cloud>
2019-01-10 11:02:57 +0100

    Eliminate le classi "factory" del package : cloud.jgo.utils.command.terminal.phase, poichè ritenute irrilevanti, dal momento che esiste soltanto un tipo concreto di fase, per il momento.

--- jGo/src/test/LocalPhaseTerminalTest2.java
+++ jGo/src/test/LocalPhaseTerminalTest2.java
@@ -5,15 +5,13 @@ import cloud.jgo.utils.command.execution.Execution;
 import cloud.jgo.utils.command.terminal.phase.LocalPhaseTerminal;
 import cloud.jgo.utils.command.terminal.phase.Phase;
 
+
 public class LocalPhaseTerminalTest2 {
 public static void main(String[] args) {
 	
 	
-	
-	// Primissima cosa, tutti i metodi della classe LocalPhaseTerminal che restituiscono una fase localPhase deve restituire
-	// una Phase generica, altrimenti subentrano problemi di visibilità
 	// 1 se creiamo una fase tramite la factory, eppoi l'aggiungiamo al terminale in un qualche modo che succede ?
-	// 2 vogliamo determinare se le fasi possono essere eseguite sempre oppure
+	// 2 vogliamo determinare se le fasi possono essere eseguite sempre oppure solo se soddisfatte
 	
 	
 	LocalPhaseTerminal terminal = new LocalPhaseTerminal("mio");
@@ -25,67 +23,7 @@ public static void main(String[] args) {
 	// mi creo una fase tramite la factory
 	
 	
-	// adesso vediamo come posso creare una fase
-	Phase fase1 = terminal.createPhase(1, "connection", "connection phase");
-	Phase fase2 = terminal.createPhase(2, "end", "end phase");
-	
-	
-	// okok mi sono creato la fase, ora però mi creo i comandi che dovranno far parte di questa fase
-	
-	LocalCommand connect,migrate,download,update ;
-	
-	connect = new LocalCommand("connect", "connect");
-	migrate = new LocalCommand("migrate", "migrate");
-	download = new LocalCommand("download", "download");
-	update = new LocalCommand("update", "update");
-	
-	
-	// bene, adesso do una esecuzione a ciascuno, come se non avessimo l'esecuzione condivisa
-	
-	connect.setExecution(new Execution() {
 		
-		@Override
-		public Object exec() {
-			// TODO Auto-generated method stub
-			return "Connect OK";
-		}
-	});
-	
-	migrate.setExecution(new Execution() {
-		
-		@Override
-		public Object exec() {
-			// TODO Auto-generated method stub
-			return "Migrate OK";
-		}
-	});
-	download.setExecution(new Execution() {
-		
-		@Override
-		public Object exec() {
-			// TODO Auto-generated method stub
-			return "Download OK";
-		}
-	});
-	update.setExecution(new Execution() {
-		
-		@Override
-		public Object exec() {
-			// TODO Auto-generated method stub
-			return "Update OK";
-		}
-	});
-	
-	
-	// bene a questo punto posso aggiungere i comandi alla fase tramite terminale
-	
-	terminal.addCommandsToPhase(fase1, connect,migrate,download,update);
-	
-	
-	// bene posso attivare il terminale 
-	
-	terminal.open();
-	
 	
 }
 }
