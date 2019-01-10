dc7d3c807944df0ca322730ac50e0c1ff7e7f39b
jGo-java-General-operations <staff@jgo.cloud>
2019-01-10 17:54:41 +0100

    Controllo esecuzione di fase - 2 test positivo

--- jGo/src/test/LocalPhaseTerminal3.java
+++ jGo/src/test/LocalPhaseTerminal3.java
@@ -16,7 +16,10 @@ import cloud.jgo.utils.command.terminal.phase.Rule;
 public class LocalPhaseTerminal3 {
 public static void main(String[] args) {
 	
-	// controllo di esecuzione sulla fase 
+	// controllo di esecuzione sulla fase :
+	// adesso vogliamo provare a bloccare l'esecuzione
+	// di un comando e tutti i suoi parametri, fin quando
+	// la fase non risulta soddisfatta
 	
 	LocalPhaseTerminal t = new LocalPhaseTerminal("test-trm");
 	
@@ -24,60 +27,71 @@ public static void main(String[] args) {
 	
 	LocalCommand.setInputHelpExploitable(true);
 	
+	// mi creo i comandi:
+	
+		LocalCommand objCommand = LocalCommand.getCommandByObject(Persona.class);
+		LocalCommand blocked = new LocalCommand("blocked","blocked help");
+		// qui imposto che questo comando deve essere eseguito solo
+		// quando la fase a cui è associato è soddisfatta
+		blocked.validExecution(When.IF_SATISFIED);
+		blocked.setExecution(new Execution() {
+			
+			@Override
+			public Object exec() {
+				// TODO Auto-generated method stub
+				return "blocked execution";
+			}
+		});
+		// mi creo qualche parametro di blocked
+		Parameter p,p2 ;
+		p = blocked.addParam("a","a help");
+		p2 = blocked.addParam("b","b help");
+		p.validExecution(When.IF_SATISFIED);
+		p2.validExecution(When.IF_SATISFIED);
+		
+		p.setExecution(new Execution() {
+			
+			@Override
+			public Object exec() {
+				// TODO Auto-generated method stub
+				return "a execution";
+			}
+		});
+		
+		p2.setExecution(new Execution() {
+			
+			@Override
+			public Object exec() {
+				// TODO Auto-generated method stub
+				return "b execution";
+			}
+		});
+	
 	// mi creo la prima fase
 	
-	Phase phase = t.createPhase(1, "connection", "connection phase");
-	phase.validExecution(When.IF_SATISFIED);
+	Phase phase = t.createPhase(1, "creation", "creation phase");
 	phase.satisfiableThrough(new Rule() {
 		
 		@Override
 		public boolean verification() {
-			return false ;
+			if (objCommand.getSharedObject()!=null) {
+				Persona persona = objCommand.getSharedObject();
+				if (persona.getNome()!=null&&persona.getCognome()!=null&&persona.getEtà()>18)return true ;
+				else return false ;
+			}
+			else return false ;
 		}
 		
 		@Override
 		public String ruleExplanation() {
 			// TODO Auto-generated method stub
-			return "default";
+			return "Se la persona è stata configurata correttamente";
 		}
 	});
 	
-	// mi creo i comandi di questa fase 
-	
-	LocalCommand connect = new LocalCommand("connect", "connect");
-	connect.validExecution(When.IF_ACCESSIBLE);
-	LocalCommand migrate = new LocalCommand("migrate", "migrate");
-	migrate.validExecution(When.IF_ACCESSIBLE);
-	LocalCommand download = new LocalCommand("download", "download");
-	download.validExecution(When.IF_ACCESSIBLE);
-	LocalCommand update = new LocalCommand("update", "update");
-	update.validExecution(When.IF_ACCESSIBLE);
-	
-	
-	SharedExecution execution = new SharedExecution() {
-		
-		@Override
-		protected Object sharedExec(Sharer sharer) {
-			
-			if (sharer.getSharerType()==Sharer.Type.COMMAND) {
-				
-				Command source = (Command) sharer;
-				
-				return source.getCommand()+" successfully (OK)";							
-			}
-			else return null ;
-		}
-	};
-	
-	connect.setExecution(execution);
-	migrate.setExecution(execution);
-	download.setExecution(execution);
-	update.setExecution(execution);
-
-	
 	// aggiungo i comandi alla fase
 	
-	t.addCommandsToPhase(phase, connect,migrate,download,update);
+	t.addCommandsToPhase(phase, objCommand,blocked);
 	
 	
 	t.open();
